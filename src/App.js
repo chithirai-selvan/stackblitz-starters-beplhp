4694ce7ee7f1015fd4c9231f6ce28c2670c5a77e
chithirai-selvan <126243751+chithirai-selvan@users.noreply.github.com>
2023-07-17 20:33:01 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>CHITHIRAI</h1>
+      <h1>CHITHIRAI 2</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
